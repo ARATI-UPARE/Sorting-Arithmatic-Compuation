61026dd85b682967e162f2af0c07e50ee9a0ea65
ARATI-UPARE <aratiupare@gmail.com>
2020-03-18 10:34:01 +0530

    ADDED:Find result of first equation

--- Arithmatic.sh
+++ Arithmatic.sh
@@ -1,7 +1,12 @@
 #! /bin/bash  -x
 
-# WAP to take read three numbers from user.
+# WAP to take read three numbers from user and find result of arithmatic operations.
 read -p "Enter Three Numbers " a b c
 echo "a=$a b=$b c=$c "
+
+#Computation of a+b*c
+
+result1=$(( a + b * c ))
+echo "Result1=$result1"
 
 
