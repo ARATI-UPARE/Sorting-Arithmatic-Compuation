3aacf36fe8b352d2b16ca5a1f3d8658d33891a3d
ARATI-UPARE <aratiupare@gmail.com>
2020-03-20 21:30:56 +0530

    ADDED:Sorting array in Desending order

--- Arithmatic.sh
+++ Arithmatic.sh
@@ -4,6 +4,7 @@
 # find result of arithmatic operations.
 # Add all results in dictionary.
 # Add array to read result from dictionary.
+# Sort result in Desending order
 
 declare -A dict
 declare -a array
@@ -12,19 +13,19 @@ read -p "Enter Three Numbers " a b c
 echo "a=$a b=$b c=$c "
 
 #Computation of a+b*c
-result1=$(( a + b * c ))
+result1=$(( $a + $b * $c ))
 echo "Result1=$result1"
 
 # Equation2 (a*b+c)
-result2=$(( a * b + c ))
+result2=$(( $a * $b + $c ))
 echo "Result2=$result2"
 
 # Equation3 (c+a/b)
-result3=`echo "scale=2; $c + $a / $b " | bc`
+result3=`echo "scale=1;  $c + $a / $b " | bc`
 echo "Result3=$result3"
 
 # Equation4 (a%b+c)
-result4=`echo "scale=2; $a % $b + $c " | bc`
+result4=`echo "scale=1; $a % $b + $c " | bc`
 echo "Result4=$result4"
 
 # Dictionary to store result
@@ -39,3 +40,25 @@ do
 				(( count++ ))
 done
 echo ${array[@]}
+
+# Sorting array in Desending order
+temp=0
+length=${#array[@]}        # length of array
+
+for(( outLoop=0; outLoop<=$length; outLoop++ ))
+do
+		for(( inLoop=$(( $outLoop + 1 )); inLoop<=$length; inLoop++ ))
+		do
+				
+				if [[ ${array[outLoop]%%.*} -lt ${array[inLoop]%%.*} ]]
+				then
+						temp="${array[$outLoop]}"
+						array[$outLoop]="${array[$inLoop]}"
+						array[$inLoop]=$temp
+				fi
+		done
+done
+
+echo "Sorted Array in Desending order :"
+echo "${array[@]}"
+
